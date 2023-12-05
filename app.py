c73f9e2d3266369cc9fd5d51ab9c36af6714f63b
Minhyeok Seo <minh0129@naver.com>
2023-12-05 14:19:12 +0900

    hello_world

--- app.py
+++ app.py
@@ -1,35 +1,36 @@
-import streamlit as st
-data = [1114, 1234, 423]
-st.write('# Hello World')
-st.write('\n## raw')
-data
-st.write('## bar chart')
-st.bar_chart(data)
+# import streamlit as st
+# data = [1114, 1234, 423]
+# st.write('# Hello World')
+# st.write('\n## raw')
+# data
+# st.write('## bar chart')
+# st.bar_chart(data)
 
-import pandas as pd
-sview = pd.Series(data)
-sview
+# import pandas as pd
+# sview = pd.Series(data)
+# sview
 
 # st.write('## Fuck You')
 
-# import streamlit as st
+import streamlit as st
 
-# # 세션 상태 초기화
-# if 'user_text' not in st.session_state:
-#     st.session_state.user_text = ""
+# 세션 상태 초기화
+if 'user_text' not in st.session_state:
+    st.session_state.user_text = ""
 
-# def main():
-#     # 제목 추가
-#     st.title("와라 편의점")
+def main():
+    # 제목 추가
+    st.title("와라 편의점")
+    st.write("# 자취생의 건강한 한 끼")
 
-#     # 텍스트 상자 추가
-#     user_input = st.text_input("여기에 텍스트를 입력하세요:", "기본 텍스트")
+    # 텍스트 상자 추가
+    user_input = st.text_input("여기에 텍스트를 입력하세요:", "기본 텍스트")
 
-#     # 입력한 텍스트를 세션 상태에 저장
-#     st.session_state.user_text = user_input
+    # 입력한 텍스트를 세션 상태에 저장
+    st.session_state.user_text = user_input
 
-#     # 세션 상태에 저장된 텍스트 출력
-#     st.write("입력한 텍스트:", st.session_state.user_text)
+    # 세션 상태에 저장된 텍스트 출력
+    st.write("입력한 텍스트:", st.session_state.user_text)
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()

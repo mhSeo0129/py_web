5e7328d8257b44db0b980215f923a281cf5b8743
jw <wjddn5193@gmail.com>
2023-12-06 00:18:55 +0900

    make program  sequence. need font and design change

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import streamlit as st
-import pandas as pd
 
 # 전체 페이지 설정 (이 부분을 가장 위로 옮깁니다.)
 st.set_page_config(page_title="와라, 편의점", page_icon="🍱", layout="wide")
 
-with open("style.css") as css:
-    st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
 
 # 왼쪽 상단 로고 (크기 조절)
 st.image("logo.png", use_column_width=False, width=300)
 
+
+# 중앙에 텍스트 (가운데 정렬) 및 마진 추가
+st.markdown("""
+    <h2 style='text-align: center; margin-top: 50px; margin-bottom: 250px; color: black;'>
+        자취생을 위한 건강한 한 끼
+    </h2>
+""", unsafe_allow_html=True)
+
+
 # 중앙에 텍스트 (가운데 정렬)
-st.title("자취생을 위한 건강한 한끼")
+
 
 sidebar_deco = st.sidebar.selectbox(
     'Pages',
@@ -19,18 +25,33 @@ sidebar_deco = st.sidebar.selectbox(
 
 from streamlit_extras.switch_page_button import switch_page
 
-if st.button("시작하기"):
-    switch_page('start')
+button_container = st.container()
+with button_container:
+    if st.button("시작하기"):
+        switch_page('start')
 
-st.write("My name is jeongwoo Kim")
 
-custom_css = {
-    '@font-face': {
-        'font-family': '"CustomFont"',
-        'src': 'url("/fonts/mhSeo.ttf") format("truetype")'
-    },
+# 스타일을 적용하여 가운데 정렬
+button_container.markdown("""
+    <style>
+        div.stButton {
+            display: flex;
+            justify-content: center;
+        }
+    </style>
+""", unsafe_allow_html=True)
 
-    '[class^="ag-"]:not([class^="ag-icon"])': {
-        'font-family': '"CustomFont"'
-    }
-}
+
+# custom_css = f"""
+#     @font-face {{
+#         font-family: 'CustomFont';
+#         src: url("./fonts/mhSeo.ttf") format("truetype");
+#     }}
+
+#     body {{
+#         font-family: 'CustomFont', sans-serif;
+#     }}
+# """
+
+
+# st.markdown(f'<style>{custom_css}</style>', unsafe_allow_html=True)
--- pages/menu.py
+++ pages/menu.py
@@ -3,8 +3,17 @@ import streamlit as st
 # 전체 페이지 설정
 st.set_page_config(page_title="식단 구성", page_icon="🍱", layout="wide")
 
-if 'user_name' not in st.session_state:
-    st.session_state['user_name'] = ''
-else: 
-    name = st.session_state['user_name']
-    st.subheader(name + "님, 식단 구성 페이지에 오신 것을 환영합니다.")
+# 중앙에 텍스트 (가운데 정렬)
+st.markdown("""
+    <h1 style='text-align: center; margin-top: 50px; margin-bottom: 50px; color: black;'>
+        식단 구성
+    </h1>
+""", unsafe_allow_html=True)
+
+budget = st.session_state.get('budget', [])
+# Streamlit 세션 상태에서 선택한 값을 가져옴
+selected_categories = st.session_state.get('selected_categories', [])
+
+# 선택한 값 출력
+st.write(f"예산: {budget}")
+st.write(f"선택한 음식 종류: {selected_categories}")
